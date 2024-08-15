647c750e8a956882f3861ad5e36152e84998b790
Chaos-19 <kalgetachew375@gmail.com>
2024-08-15 20:37:04 +0300

    shad check box component added

--- src/hooks/fetchJsonData.ts
+++ src/hooks/fetchJsonData.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 
 const loadData = async (filePath: string) => {
   const response = await import(/* @vite-ignore */ `../assets/${filePath}`);
